81b0e74c1251f7236b6afdeb3021721cd66d74fb
javf1016 <ja.vera@uniandes.edu.co>
2023-04-20 17:34:55 -0500

    javf1016 create project
    Create Project Initzr

---
+++ src/main/java/com/example/construequiposmusk/ConstruEquiposMuskApplication.java
@@ -0,0 +1,13 @@
+package com.example.construequiposmusk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstruEquiposMuskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConstruEquiposMuskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/construequiposmusk/ConstruEquiposMuskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.construequiposmusk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstruEquiposMuskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
